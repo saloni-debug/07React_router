ff274f79d72f9e715faf32211696254e6b5d76bb
Saloni180430 <156230375+Saloni180430@users.noreply.github.com>
2024-03-30 22:52:53 +0530

    React router project

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,17 +1,24 @@
 import React, { useState, useEffect } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-    const [data, setData] = useState("")
-    useEffect (() => {
-        fetch('https://api.github.com/users/hiteshchoudhary')
-        .then(res => res.json())
-        .then(data => setData(data))
-    })
+    const data = useLoaderData()
+    // const [data, setData] = useState("")
+    // useEffect (() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(res => res.json())
+    //     .then(data => setData(data))
+    // })
   return (
-    <div className='bg-gray-300 m-4 text-3xl text-white text-center;'>
+    <div className='bg-gray-600 m-4 text-3xl text-white text-center;'>
       Github followers: {data.followers}
     </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+  const res = await fetch('https://api.github.com/users/hiteshchoudhary')
+  return res.json()
+}
